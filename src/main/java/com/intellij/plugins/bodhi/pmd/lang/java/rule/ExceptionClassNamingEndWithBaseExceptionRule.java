ba4815b9ac46e2a9722488452c8d73de2375a188
zhaojingbo <zjbhnay@163.com>
2024-07-27 16:11:25 +0800

    test名规则

--- src/main/java/com/intellij/plugins/bodhi/pmd/lang/java/rule/ExceptionClassNamingEndWithBaseExceptionRule.java
+++ src/main/java/com/intellij/plugins/bodhi/pmd/lang/java/rule/ExceptionClassNamingEndWithBaseExceptionRule.java
@@ -12,7 +12,7 @@ public class ExceptionClassNamingEndWithBaseExceptionRule extends ClassNamingCon
     public Object visit(ASTClassDeclaration node, Object data) {
 //        todo 没解决 Ex1 extends Ex extends RuntimeException 中 EX1的校验问题
         ASTClassType superClassTypeNode = node.getSuperClassTypeNode();
-        if (Objects.equals(superClassTypeNode.getSimpleName(), RuntimeException.class.getSimpleName())) {
+        if (Objects.nonNull(superClassTypeNode) && Objects.equals(superClassTypeNode.getSimpleName(), RuntimeException.class.getSimpleName())) {
             if (node.getSimpleName().endsWith("Exception")) {
                 asCtx(data).addViolation(node, superClassTypeNode.getSimpleName());
             }
---
+++ src/main/java/com/intellij/plugins/bodhi/pmd/lang/java/rule/TestClassShouldEndWithTestNamingRule.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 1999-2017 Alibaba Group.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.intellij.plugins.bodhi.pmd.lang.java.rule;
+
+import net.sourceforge.pmd.lang.java.ast.ASTMethodDeclaration;
+import net.sourceforge.pmd.lang.java.rule.codestyle.MethodNamingConventionsRule;
+import net.sourceforge.pmd.lang.java.rule.internal.TestFrameworksUtil;
+
+public class TestClassShouldEndWithTestNamingRule extends MethodNamingConventionsRule {
+    private static final String TEST_SUFFIX = "Test";
+
+    @Override
+    public Object visit(ASTMethodDeclaration node, Object data) {
+        if (TestFrameworksUtil.isJUnit5Method(node) || TestFrameworksUtil.isJUnit4Method(node) || TestFrameworksUtil.isJUnit3Method(node)) {
+            if (!node.getName().endsWith(TEST_SUFFIX)) {
+                asCtx(data).addViolation(node, node.getName());
+            }
+        }
+        return super.visit(node, data);
+    }
+}

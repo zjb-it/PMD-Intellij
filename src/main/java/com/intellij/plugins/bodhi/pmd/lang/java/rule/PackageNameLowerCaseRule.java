70e2a3405a88b48d4755b8cd2d96840cc204e00e
zhaojingbo <zjbhnay@163.com>
2024-07-29 09:08:17 +0800

    规则-package name

---
+++ src/main/java/com/intellij/plugins/bodhi/pmd/lang/java/rule/PackageNameLowerCaseRule.java
@@ -0,0 +1,19 @@
+package com.intellij.plugins.bodhi.pmd.lang.java.rule;
+
+import net.sourceforge.pmd.lang.java.ast.ASTPackageDeclaration;
+import org.apache.commons.lang3.StringUtils;
+
+public class PackageNameLowerCaseRule extends AbstractLuBanRule {
+    public PackageNameLowerCaseRule() {
+        super(ASTPackageDeclaration.class);
+    }
+
+    @Override
+    public Object visit(ASTPackageDeclaration node, Object data) {
+        String name = node.getName();
+        if (!StringUtils.isAllLowerCase(name.replace(".",""))) {
+            asCtx(node).addViolation(node, name);
+        }
+        return data;
+    }
+}
--- todo.md
+++ todo.md
@@ -1,2 +1,5 @@
 # 实时检测
 https://plugins.jetbrains.com/docs/intellij/code-inspections.html#inspection-implementation-java-class
+
+过早定义变量
+net.sourceforge.pmd.lang.java.rule.codestyle.PrematureDeclarationRule

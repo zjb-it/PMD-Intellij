95c22373d3d7f303ce4e11b7fc3a3b2a93e97ba5
zhaojingbo <zjbhnay@163.com>
2024-07-30 14:59:02 +0800

    规则-oop rule-【强制】所有的覆写方法，必须加@Override 注解。

---
+++ src/main/java/com/intellij/plugins/bodhi/pmd/lang/java/rule/oop/OverrideMethodMustOverrideAnnotationRule.java
@@ -0,0 +1,24 @@
+package com.intellij.plugins.bodhi.pmd.lang.java.rule.oop;
+
+import com.intellij.plugins.bodhi.pmd.lang.java.rule.naming.AbstractLuBanRule;
+import net.sourceforge.pmd.lang.ast.NodeStream;
+import net.sourceforge.pmd.lang.java.ast.ASTMethodCall;
+import net.sourceforge.pmd.lang.java.ast.ASTMethodDeclaration;
+import net.sourceforge.pmd.lang.java.ast.ASTTypeExpression;
+import net.sourceforge.pmd.lang.java.ast.JavaNode;
+
+public class OverrideMethodMustOverrideAnnotationRule extends AbstractLuBanRule {
+    public OverrideMethodMustOverrideAnnotationRule() {
+        super(ASTMethodDeclaration.class);
+    }
+
+    @Override
+    public Object visit(ASTMethodDeclaration node, Object data) {
+        if (node.isOverridden()) {
+            if (!node.isAnnotationPresent(Override.class)) {
+                addViolation(node, data, node.getName());
+            }
+        }
+        return super.visit(node, data);
+    }
+}

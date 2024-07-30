d81ac40b4ceaf8012c6d456085e10d228a842e92
zhaojingbo <zjbhnay@163.com>
2024-07-30 16:27:21 +0800

    oop rule-【强制】禁止在 POJO 类中，同时存在对应属性 xxx 的 isXxx()和 getXxx()方法

---
+++ src/main/java/com/intellij/plugins/bodhi/pmd/lang/java/rule/oop/AvoidIsGetMethodCoexistRule.java
@@ -0,0 +1,36 @@
+package com.intellij.plugins.bodhi.pmd.lang.java.rule.oop;
+
+import com.intellij.plugins.bodhi.pmd.lang.java.rule.naming.AbstractLuBanRule;
+import net.sourceforge.pmd.lang.ast.NodeStream;
+import net.sourceforge.pmd.lang.java.ast.*;
+import net.sourceforge.pmd.lang.java.ast.internal.JavaAstUtils;
+import net.sourceforge.pmd.lang.java.rule.internal.JavaRuleUtil;
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class AvoidIsGetMethodCoexistRule extends AbstractLuBanRule {
+    public AvoidIsGetMethodCoexistRule() {
+        super(ASTClassDeclaration.class);
+    }
+
+    @Override
+    public Object visit(ASTClassDeclaration node, Object data) {
+        NodeStream<ASTMethodDeclaration> declarations = node.getDeclarations(ASTMethodDeclaration.class);
+        Map<String, List<@NonNull ASTMethodDeclaration>> map = declarations.toStream()
+                .filter(JavaRuleUtil::isGetterOrSetter)
+                .collect(Collectors.groupingBy(declaration -> declaration.getName().replace("is", "").replace("get", "")));
+        map.forEach((k, methods) -> {
+            if (methods.size() > 1) {
+                methods.forEach(method -> {
+                    addViolation(method, data, method.getName());
+                });
+            }
+        });
+        return super.visit(node, data);
+    }
+
+
+}
--- todo.md
+++ todo.md
@@ -8,6 +8,10 @@ net.sourceforge.pmd.lang.java.rule.codestyle.PrematureDeclarationRule
 [//]: # (pmd不支持)
 【强制】不能使用过时的类或方法。
 
+（十二）【强制】构造方法里面禁止加入任何业务逻辑，如果有初始化逻辑，请放在 init 方法中
+
+（十三）【强制】POJO 类必须写 toString 方法。使用 IDE 中的工具：source> generate toString时，如果继承了另一个 POJO 类，注意在前面加一下 super.toString。
+
 
 JavaAstUtils
 TypeTestUtil

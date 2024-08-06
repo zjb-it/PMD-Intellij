55ed4582600d9e78672fcc77488e9aab1190cafe
zhaojingbo <zjbhnay@163.com>
2024-08-06 11:59:08 +0800

    （四）【强制】线程池不允许使用 Executors 去创建是通ThreadPoolExecutor 的方式

---
+++ src/main/java/com/intellij/plugins/bodhi/pmd/lang/java/rule/concurrent/ThreadPoolCreationRule.java
@@ -0,0 +1,27 @@
+
+package com.intellij.plugins.bodhi.pmd.lang.java.rule.concurrent;
+
+import com.intellij.plugins.bodhi.pmd.lang.java.rule.naming.AbstractLuBanRule;
+import net.sourceforge.pmd.lang.java.ast.ASTMethodCall;
+import net.sourceforge.pmd.lang.java.types.TypeTestUtil;
+
+import java.util.Set;
+import java.util.concurrent.Executors;
+
+public class ThreadPoolCreationRule extends AbstractLuBanRule {
+
+    private static final Set<String> CHECK_METHOD_NAME = Set.of("newFixedThreadPool","newCachedThreadPool","newSingleThreadExecutor","newSingleThreadExecutor");
+    public ThreadPoolCreationRule() {
+        super(ASTMethodCall.class);
+    }
+
+    @Override
+    public Object visit(ASTMethodCall node, Object data) {
+        if (TypeTestUtil.isA(Executors.class, node.getMethodType().getDeclaringType())) {
+            if (CHECK_METHOD_NAME.contains(node.getMethodName())) {
+                addViolation(data,node,node.getMethodName());
+            }
+        }
+        return super.visit(node, data);
+    }
+}
